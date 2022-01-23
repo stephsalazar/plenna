f5f7eb3120aa9292283e3c8541db860662dd97d2
stephSalazar <anastephany.salazarguzman@gmail.com>
2022-01-22 23:13:19 -0600

    feat(src): Add view of details and required components

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,53 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-import Login from './pages/Login'
-import Detail from './pages/Detail'
-import Consult from './pages/Consult'
-import Closing from './pages/Closing'
-import NotFound from './pages/NotFound'
+import Login from './pages/Login';
+import Detail from './pages/Detail';
+import Consult from './pages/Consult';
+import Closing from './pages/Closing';
+import NotFound from './pages/NotFound';
 
+const patient = {
+  isLogged: true,
+  name: 'Verónica Guzmán',
+  age: 32,
+  weight: 55.200,
+  height: 152,
+  allergies: 'Penicilina',
+  medicalHistory: [
+    {
+      date: '12 / Octubre / 2021',
+      resume: 'Consulta Básica',
+    },
+    {
+      date: '20 / Diciembre / 2021',
+      resume: 'Papanicolau',
+    },
+    {
+      date: '15 / Enero / 2022',
+      resume: 'Revisión DIU',
+    },
+  ],
+  nextVisits: [
+    {
+      date: '25 / Marzo / 2022',
+      resume: 'Lectura de estudios',
+    },
+    {
+      date: '12 / Mayo / 2022',
+      resume: 'Retiro del DIU',
+    },
+  ],
+};
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route path="/detail" component={Detail} />
+        <Route path="/detail">
+          <Detail patient={patient} />
+        </Route> 
         <Route path="/consult" component={Consult} />
         <Route path="/closing" component={Closing} />
         <Route path="*" component={NotFound} />
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import patientImage from '../assets/images/patient.jpeg';
+import '../styles/detail.css';
+
+const Profile = props => {
+  const {
+    name,
+    age,
+    weight,
+    height,
+    allergies,
+  } = props.data;
+
+  return (
+    <section className='profile'>
+      <img src={patientImage} alt='patient' width={'80%'}/>
+      <p>Nombre: {name}</p>
+      <p>Edad: {age}</p>
+      <p>Peso: {weight}</p>
+      <p>Estatura: {height}</p>
+      <p>Alergias: {allergies}</p>
+    </section>
+  )
+};
+
+export default Profile;
