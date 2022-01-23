bad5b68dce5c387f82db684ec5946dad11bb2ded
stephSalazar <anastephany.salazarguzman@gmail.com>
2022-01-23 00:29:05 -0600

    feat(src): Add view of Consult and required component

---
+++ src/components/Form.jsx
@@ -0,0 +1,32 @@
+import '../styles/common.css';
+import '../styles/form.css';
+
+const Form = props => {
+  
+  return (
+    <section>
+      <h2>Formulario de consulta</h2>
+      <textarea
+        name='conditions'
+        placeholder='Padecimientos' />
+      <textarea
+        name='physicaExamination'
+        placeholder='Exploración Física'
+      />
+      <textarea
+        name='diagnostic'
+        placeholder='Diagnóstico'
+      />
+      <textarea
+        name='prescriptionAndTreatment'
+        placeholder='Receta y tratamiento'
+      />
+      <textarea
+        name='PatientNotes'
+        placeholder='Notas para la Paciente'
+      />
+    </section>
+  )
+};
+
+export default Form;
