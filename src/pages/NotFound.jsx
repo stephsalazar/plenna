ac5f231a8a6d283c8137b8cb643fcc3deefb5043
stephSalazar <anastephany.salazarguzman@gmail.com>
2022-01-23 02:56:56 -0600

    feat(src): Add view of Not Found and required component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,16 @@
+import { Link } from 'react-router-dom';
+import '../styles/common.css';
+
 const NotFound = () => {
   return (
-    <h1>NotFound</h1>
+    <section className='not-found'>
+      <h1>Oh oh!</h1>
+      <h2>Parece que has entrado a una vista que no existe</h2>
+      <h2>Pero no te preocupes, presiona el boton y lo solucionaremos</h2>
+      <Link to="/">
+          <button className="button">Click aquí</button>
+      </Link>
+    </section>
   )
 };
 
